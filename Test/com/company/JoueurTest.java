07690efeee17e971279d744eb702b384c4a53ece
Focardi François-Philippe <francois.focardi@hotmail.fr>
2022-04-01 20:51:10 +0200

    Main

--- Test/com/company/JoueurTest.java
+++ Test/com/company/JoueurTest.java
@@ -12,18 +12,21 @@ class JoueurTest {
 
     Joueur joueur = new Joueur("Francois");
     Joueur joueur2 = new Joueur("Pilou");
+
+
     @Test
     @RepeatedTest(1000)
-    @DisplayName("Test des des compris entre 1 et 6")
-    void lancerDes() {
-        joueur.lancerDes();
-        assertTrue(joueur.getLancer1() >=0 && joueur.getLancer1()<=6);
-        assertTrue(joueur.getLancer2() >=0 && joueur.getLancer2()<=6);
+    @DisplayName("Test des dés compris entre 1 et 6")
+    void lancerLesDes() {
+        joueur.lancerLesDes();
+        assertTrue(joueur.getLancer1() >0 && joueur.getLancer1()<=6);
+        assertTrue(joueur.getLancer2() >0 && joueur.getLancer2()<=6);
     }
 
 
     @Test
     @RepeatedTest(1000)
+    @DisplayName("Test pour verifier si le loyer echangé entre les joueurs est la bonne somme")
     void transactionLoyer() {
         int locataireArgents = joueur.getArgent();
         int proprietaireArgents = joueur2.getArgent();
@@ -37,6 +40,7 @@ class JoueurTest {
     }
 
     @Test
+    @DisplayName("Test d'achat de case avec meme clé et sans")
     void acheter() {
         int argentJoueur = joueur.getArgent();
         CaseProprietaire c1 = new ProprieteGare(15,"Test",100,120,"gare");
@@ -45,9 +49,13 @@ class JoueurTest {
         joueur.acheter(c1);
         assertEquals(argentJoueur-c1.prixDAchat,joueur.getArgent());
         assertTrue(joueur.casesPossedes.containsKey("gare"));
+        argentJoueur = joueur.getArgent();
         joueur.acheter(c2);
+        assertEquals(argentJoueur-c2.prixDAchat,joueur.getArgent());
         assertTrue(joueur.casesPossedes.get(c2.getCle()).size()==2);
+        argentJoueur = joueur.getArgent();
         joueur.acheter(c3);
+        assertEquals(argentJoueur-c3.prixDAchat,joueur.getArgent());
         assertTrue(joueur.casesPossedes.containsKey(c3.getCle()));
 
     }
